56728796144c3d58e5141a7b28c4d3056b9028ff
Jacob Addis <jacobaddis@Jacobs-MacBook-Air.local>
2021-10-30 20:01:27 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Scrolling_Navbar from './components/Scrolling_Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Scrolling_Navbar/>
+    </div>
+  );
+}
+
+export default App;

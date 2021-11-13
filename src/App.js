df300c234baf056d48f97abb0b4a5910ac0b1f85
Jacob Addis <jacobaddis@macbook-air.myfiosgateway.com>
2021-11-12 19:51:20 -0500

    Figured out how to use Intersection Observer API with React components :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import * as React from 'react';
-import data from './Data';
+import React, { useRef, useEffect, useState } from "react"
 
-import NavList from './components/NavList';
-import DisplayPage from './components/DisplayPage';
+function App() {
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  const box1ref = useRef(null)
+  const box2ref = useRef(null)
+  const [ BoxOneIsVisible, setBoxOneIsVisible ] = useState(false)
+  const [ BoxTwoIsVisible, setBoxTwoIsVisible ] = useState(false)
 
-    } 
+  const callbackFunction1 = (entries) => {
+    const [ entry ] = entries
+    setBoxOneIsVisible(entry.isIntersecting)
   }
-  
-  render(){
-    return(
-      <div className="App">
-        
-        <DisplayPage/>
-        <NavList/>
-        
-      </div>
-    );
-  }    
+  const callbackFunction2 = (entries) => {
+    const [ entry ] = entries
+    setBoxTwoIsVisible(entry.isIntersecting)
+  }
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold:1.0
+  }
+
+  useEffect(() => {
+    
+    const observer1 = new IntersectionObserver(callbackFunction1, options)
+    observer1.observe(box1ref.current)
+    
+    const observer2 = new IntersectionObserver(callbackFunction2, options)
+    observer2.observe(box2ref.current)
+
+  }, [box1ref, box2ref, options])
+
+  return (
+    <div className="app">
+      <div className="isVisible">{BoxOneIsVisible ? "1 IS IN VIEWPORT" : BoxTwoIsVisible ? "2 IS IN VIEWPORT" : "NONE IN VIEWPORT"}</div> 
+      <div className="section"></div>
+     <div className="box" ref={box1ref}>Box1</div>
+     <div className="box" ref={box2ref}>Box2</div>
+    </div>
+  );
 }
+
 export default App;
